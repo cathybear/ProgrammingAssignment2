809105a9bff11f7ada0f15f29b8b7d820ba3cfe8
cathybear <x.cathy01@gmail.com>
2018-09-08 16:44:35 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## set vector
+            m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+## get vector
+      get <- function()x
+      setinverse <- function(mean) m <<- mean
+      getinverse <- function() m
+      matrix(set = set, 
+             get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
+
+
 
 
 ## Write a short comment describing this function
